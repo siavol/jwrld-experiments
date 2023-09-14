49f3a5f9025d78d1f45a077532261b6cedeb5cae
Ilya Shestakov <siavol@gmail.com>
2023-09-14 21:34:28 +0300

    add sample rest controller

--- src/main/kotlin/me/siavol/springbootkotlin/SpringBootKotlinApplication.kt
+++ src/main/kotlin/me/siavol/springbootkotlin/SpringBootKotlinApplication.kt
@@ -2,10 +2,19 @@ package me.siavol.springbootkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SpringBootKotlinApplication
 
 fun main(args: Array<String>) {
     runApplication<SpringBootKotlinApplication>(*args)
+}
+
+@RestController
+class MessageController {
+    @GetMapping("/")
+    fun index(@RequestParam("name") name: String) = "Hello, $name"
 }
