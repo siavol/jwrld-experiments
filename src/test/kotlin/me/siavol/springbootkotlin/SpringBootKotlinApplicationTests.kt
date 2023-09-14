d55080cfb6dd3a080d4aa4386d66953b58107a7b
Ilya Shestakov <siavol@gmail.com>
2023-09-14 20:56:19 +0300

    Created from spring initializr

---
+++ src/test/kotlin/me/siavol/springbootkotlin/SpringBootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.siavol.springbootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
