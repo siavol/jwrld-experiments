230e152ec8ef7739c39878b3d3198a06934048ab
Ilya Shestakov <siavol@gmail.com>
2023-09-16 17:32:09 +0300

    Send some traces

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
--- src/main/kotlin/me/siavol/springbootkotlin/SpringBootKotlinApplication.kt
+++ src/main/kotlin/me/siavol/springbootkotlin/SpringBootKotlinApplication.kt
@@ -1,9 +1,13 @@
 package me.siavol.springbootkotlin
 
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 @EnableConfigurationProperties(BlogProperties::class)
@@ -12,5 +16,13 @@ class SpringBootKotlinApplication
 fun main(args: Array<String>) {
     runApplication<SpringBootKotlinApplication>(*args) {
         setBannerMode(Banner.Mode.OFF)
+    }
+}
+
+@Configuration(proxyBeanMethods = false)
+class MyConfiguration {
+    @Bean
+    fun otlpHttpSpanExporter(@Value("\${tracing.url}") url: String?): OtlpGrpcSpanExporter {
+        return OtlpGrpcSpanExporter.builder().setEndpoint(url!!).build()
     }
 }
